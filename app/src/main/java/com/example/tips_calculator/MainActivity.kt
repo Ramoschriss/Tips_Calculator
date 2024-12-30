75fc3c406b33f5b13c064945a8adea724a730892
chris <ramoschris901@gmail.com>
2024-12-30 15:34:23 +0000

    Parte visual finalizada.

--- app/src/main/java/com/example/tips_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tips_calculator/MainActivity.kt
@@ -1,25 +1,19 @@
 package com.example.tips_calculator
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Adapter
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.tips_calculator.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,37 +22,50 @@ class MainActivity : AppCompatActivity() {
 
         var percentage: Int = 0
         binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
-
             if (isChecked) {
                 percentage = 10
             }
-
         }
         binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
-
             if (isChecked) {
                 percentage = 15
             }
-
         }
         binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
-
             if (isChecked) {
                 percentage = 20
             }
-
         }
 
+        // Definir cores e itens para o spinner
+        val colors = listOf("#FF0000", "#00FF00", "#0000FF", "#FFA500", "#8B4513","#F5DEB3", "#7B68EE", "#8B008B", "#FF1493", "#800000" )
+        val items = listOf("1 pessoa", "2 pessoas ", "3 pessoas ", "4 pessoas", "5 pessoas", "6 pessoas", "7 pessoas", "8 pessoas", "9 pessoas", "10 pessoas")
 
-        val adapter = ArrayAdapter.createFromResource(
-            this, R.array.num_people,
-            android.R.layout.simple_spinner_item
-        )
+        // Criar o adapter personalizado
+        val customAdapter = object : ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_item,
+            items
+        ) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getView(position, convertView, parent) as TextView
+                view.text = items[position]
+                view.setTextColor(Color.parseColor(colors[position]))
+                return view
+            }
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
-        binding.spinnerNumberOfPeople.adapter = adapter
+            override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getDropDownView(position, convertView, parent) as TextView
+                view.text = items[position]
+                view.setTextColor(Color.parseColor(colors[position]))
+                return view
+            }
+        }
 
-        var numOfPeopleSelected = 0
+        // Configurar o adapter personalizado no spinner
+        binding.spinnerNumberOfPeople.adapter = customAdapter
+
+        var numOfPeopleSelected = 1 // Inicializar como 1 para evitar divisão por zero
 
         binding.spinnerNumberOfPeople.onItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
@@ -68,41 +75,26 @@ class MainActivity : AppCompatActivity() {
                     position: Int,
                     id: Long
                 ) {
-                     numOfPeopleSelected = position
-
+                    numOfPeopleSelected = position + 1 // +1 para representar "1 pessoa"
                 }
 
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-
-                }
-
-
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
 
         binding.btnDone.setOnClickListener {
             val totalTableTemp = binding.tieTotal.text
 
-
-
-            if (totalTableTemp?.isEmpty() == true
-            ) {
+            if (totalTableTemp?.isEmpty() == true) {
                 Snackbar.make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_LONG)
                     .show()
-
             } else {
                 val totalTable: Float = totalTableTemp.toString().toFloat()
-                val nPeolpe: Int = numOfPeopleSelected
-
-                val totalTemp = totalTable / nPeolpe
+                val totalTemp = totalTable / numOfPeopleSelected
                 val tips = totalTemp * percentage / 100
                 val totalWithTips = totalTemp + tips
-                binding.tvResult.text = "Total with tips: $totalWithTips"
-
+                binding.tvResult.text = "Total with tips: R$%.2f".format(totalWithTips)
             }
-
-
         }
-
 
         binding.btnClean.setOnClickListener {
             binding.tvResult.text = ""
@@ -110,10 +102,6 @@ class MainActivity : AppCompatActivity() {
             binding.rbOptionThree.isChecked = false
             binding.rbOptionOne.isChecked = false
             binding.rbOptionTwo.isChecked = false
-
-
         }
-
-
     }
 }
